24c108fa91aa34271a5d4a6aee72649f12bb79f6
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-01-19 08:54:45 +0500

    change all

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ document.getElementById('modeBtn').addEventListener('click', function () {
     a = 0;
   }
 });
+
+let searchbtn = document.getElementById('search');
+let removebtn = document.getElementById('remove');
+let modalboxbtn = document.getElementById('modalbox');
+
+searchbtn.addEventListener('click', function() {
+  modalbox.classList.add('show');
+});
+
+removebtn.addEventListener('click', function() {
+  modalbox.classList.remove('show');
+});
